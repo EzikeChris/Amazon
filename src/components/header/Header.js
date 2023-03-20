721856f7bb30922de904749c5c2bc025f69c1104
ChrisEzikeJL <chris@joint.cool>
2023-03-21 00:47:18 +0100

    chore: initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { GrLocation } from "react-icons/gr";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "../stateProvider/StateProvider";
+import { auth } from "firebase";
+
+function Header() {
+  const [{ basket, user }] = useStateValue();
+  const history = useHistory();
+  console.log(basket);
+  console.log(user);
+
+  const login = () => {
+    if (user) {
+      auth().signOut();
+      history.push("/login");
+    }
+  };
+
+  const styles = {
+    color: " #ffffff",
+  };
+  return (
+    <nav className="header">
+      {/* logo on the left -> img */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon logo"
+        />
+      </Link>
+      <div className="new_header_title">
+        <Link className="header__link">
+          <div className="header__option">
+            <div className="header_Icon">
+              <GrLocation color="white" style={styles} />
+            </div>
+            <span className="header__optionLineOneUpdate">DeliverTo</span>
+            <span className="header__optionLineTwo">Nigeria</span>
+          </div>
+        </Link>
+      </div>
+      {/* search box */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      {/* 3 links */}
+      <div className="header__nav">
+        <Link to={!user && "/login"} className="header__link">
+          <div onClick={login} className="header__option">
+            <span className="header__optionLineOne">Hello {user?.email}</span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
+      </div>
+
+      <div className="header__nav">
+        <Link to="/order" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+      </div>
+
+      <Link to="/checkout" className="header__link">
+        <div className="header__optionBasket">
+          {/* shopping basket icon */}
+          <ShoppingBasketIcon />
+          {/* number of items in the basket */}
+          {/* we use {basket?.length} to render the length of the basket when basket property of the state reaches the header component. without this, the dom will render the basket.length even when it has not reach the header component therby resulting to an error  */}
+          <span className="header__optionLineTwo header__basketCount">
+            {basket?.length}
+          </span>
+        </div>
+      </Link>
+      {/* basket icon with number*/}
+    </nav>
+  );
+}
+
+export default Header;
